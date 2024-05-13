0937e6e1b5ab78a63e03f0093a9a9cd78c81eaa5
Mateus Colombo Godoy <mateuscgodoy@gmail.com>
2024-05-13 09:03:24 -0300

    Add custom register route that works on the blog application

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,35 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpRequest
+from django.views import View
+
+from .forms import CreateUserForm
 
 
 def index(request: HttpRequest):
     if request.method == "GET":
         return render(request, "blog/index.html")
+
+
+# For reference: https://learndjango.com/tutorials/django-login-and-logout-tutorial
+# class RegisterView(CreateView):
+#     form_class = UserCreationForm
+#     success_url = reverse_lazy("login")
+#     template_name = "registration/register.html"
+
+#     class Meta:
+#         model = get_user_model()
+
+
+class Register(View):
+    def get(self, request):
+        form = CreateUserForm()
+        context = {"form": form}
+        return render(request, "registration/register.html", context)
+
+    def post(self, request):
+        form = CreateUserForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect("login")
+        context = {"form": form}
+        return render(request, "registration/register.html", context)
